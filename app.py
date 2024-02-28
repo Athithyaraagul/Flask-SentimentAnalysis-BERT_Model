98fd85643edfd3cdae76c4fdd5480baf2ba7ec1d
Athithyaraagul Sureshkumar <113710225+Athithyaraagul@users.noreply.github.com>
2024-02-28 21:01:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 
 app = Flask(__name__)
-Bootstrap(app)  # Initialize Bootstrap
 
-# Load BERT model and tokenizer
-tokenizer = BertTokenizer.from_pretrained('/Users/Athithyaraagul/Developer/Machine_Learning/srm_researchpaper/BERT_Model_Tokenizer')
-model = BertForSequenceClassification.from_pretrained('/Users/Athithyaraagul/Developer/Machine_Learning/srm_researchpaper/BERT_SavedModel')
+# Load the pre-trained sentiment analysis model
+tokenizer = BertTokenizer.from_pretrained('BERT_SavedTokenizer')
+model = BertForSequenceClassification.from_pretrained('BERT_SavedModel', num_labels=3)
+model.eval()
 
-# Define the route for the home page
+# Function to predict sentiment
+def predict_sentiment(text):
+    inputs = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
+    outputs = model(**inputs)
+    predicted_class = torch.argmax(outputs.logits).item()
+    if predicted_class == 0:
+        sentiment = "Negative"
+    elif predicted_class == 1:
+        sentiment = "Neutral"
+    else:
+        sentiment = "Positive"
+    return sentiment
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Define the route for processing user input
-@app.route('/predict', methods=['POST'])
+@app.route('/result', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        # Get user input from the form
-        sentence = request.form['sentence']
-
-        # Tokenize and predict using BERT model
-        inputs = tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
-        outputs = model(**inputs)
-        prediction = outputs.logits.argmax(dim=1).item()
-
-        # Map the predicted label to sentiment
-        sentiments = ['Negative', 'Neutral', 'Positive']
-        predicted_sentiment = sentiments[prediction]
-
-        # Render the result template with the predicted sentiment
-        return render_template('result.html', sentence=sentence, predicted_sentiment=predicted_sentiment)
+        text = request.form['sentence']
+        sentiment = predict_sentiment(text)
+        return render_template('result.html', text=text, sentiment=sentiment)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
